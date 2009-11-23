80350a002f800cf96eb2438b69be2d638b8c0e2b
jeff <jeff@metis.(none)>
2009-11-22 20:06:00 -0500

    .

--- becked.rb
+++ becked.rb
@@ -1,6 +1,14 @@
+require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  request.inspect
+  case request.url
+    when /jeffbeck\.org/i
+      'Hi jeff.'
+    when /becked\.heroku\.com/i
+      'Hi heroku.'
+    else
+      'Hi developer.'
+  end
 end
 
