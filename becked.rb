b172a14d01ee3f8542fe4e16db59e22bcb53eab7
jeff <jeff@metis.(none)>
2009-11-22 20:12:32 -0500

    .

--- becked.rb
+++ becked.rb
@@ -3,8 +3,8 @@ require 'sinatra'
 
 get '/' do
   case request.url
-    when /jeffbeck\.org/i
     when /becked\.heroku\.com/i
+    when /jeffbeck\.org/i
       'Hi heroku.'
     else
       'Hi developer: ' +
